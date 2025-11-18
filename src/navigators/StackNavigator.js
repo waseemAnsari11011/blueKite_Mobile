9549f64e6de392e4730a225d7bf5416564dc2e97
waseemAnsari11011 <waseemahm11011@gmail.com>
2025-11-18 10:05:33 +0530

    18 nov 10:05 am

--- src/navigators/StackNavigator.js
+++ src/navigators/StackNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import OrdersScreen from '../screens/MainScreens/OrdersScreen';
 import MainTabNavigator from './MainTabNavigator';
 import DrawerNavigator from './DrawerNavigator';
@@ -15,41 +15,105 @@ import RecentlyAddedProducts from '../screens/MainScreens/RecentlyAddedProducts'
 import SearchScreen from '../screens/MainScreens/Search';
 import LocationSearch from '../screens/MainScreens/location/LocationSearch';
 import PhoneSignIn from '../screens/PhoneSignIn';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 import OrderTacking from '../screens/MainScreens/orders/OrderTacking';
 import SubmitInquiry from '../screens/MainScreens/inquiry/Inquiry';
 import faqs from '../screens/MainScreens/faqs/Faqs';
 import Contactus from '../screens/MainScreens/contact/Contactus';
 
-
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-  const { data } = useSelector(state => state?.local);
+  const {data} = useSelector(state => state?.local);
+
+  console.log('data?.user?.shippingAddresses->', data?.user?.shippingAddresses);
+  const shippingData = data?.user?.shippingAddresses;
+  const hasShippingAddress =
+    shippingData &&
+    (Array.isArray(shippingData)
+      ? shippingData.length > 0
+      : !!shippingData.addressLine1);
 
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
       }}>
-      { !data?.user?.shippingAddresses && <Stack.Screen name="Search Location" options={{ headerShown: true }} component={LocationSearch} />}
+      {!hasShippingAddress && (
+        <Stack.Screen
+          name="Search Location"
+          options={{headerShown: true, title: 'Add Delivery Address'}}
+          component={LocationSearch}
+        />
+      )}
       <Stack.Screen name="Main" component={MainTabNavigator} />
-      <Stack.Screen options={{ headerShown: true }} name="Details" component={Details} />
-      <Stack.Screen name="Edit Profile" options={{ headerShown: true }} component={EditProfile} />
-      <Stack.Screen options={{ headerShown: true }} name="CategoryProducts" component={CategoryProducts} />
+      <Stack.Screen
+        options={{headerShown: true}}
+        name="Details"
+        component={Details}
+      />
+      <Stack.Screen
+        name="Edit Profile"
+        options={{headerShown: true}}
+        component={EditProfile}
+      />
+      <Stack.Screen
+        options={{headerShown: true}}
+        name="CategoryProducts"
+        component={CategoryProducts}
+      />
       <Stack.Screen name="Edit Address" component={EditAddress} />
       <Stack.Screen name="Notifications" component={Notifications} />
-      <Stack.Screen name="My order" options={{ headerShown: true }} component={MyOrder} />
-      <Stack.Screen name="Discounted Products" options={{ headerShown: true }} component={DiscountedProducts} />
-      <Stack.Screen name="New Arrivals" options={{ headerShown: true }} component={RecentlyAddedProducts} />
-      <Stack.Screen name="Search Your Products" options={{ headerShown: true }} component={SearchScreen} />
-      <Stack.Screen name="Checkout" options={{ headerShown: true }} component={CheckoutScreen} />
-      <Stack.Screen options={{ headerShown: true }} name="otp" component={PhoneSignIn} />
-      <Stack.Screen name="Add Location" options={{ headerShown: true }} component={LocationSearch} />
-      <Stack.Screen name="Submit Inquiry" options={{ headerShown: true }} component={SubmitInquiry} />
-      <Stack.Screen name="Faqs" options={{ headerShown: true }} component={faqs} />
-      <Stack.Screen name="Contact us" options={{ headerShown: true }} component={Contactus} />
-
+      <Stack.Screen
+        name="My order"
+        options={{headerShown: true}}
+        component={MyOrder}
+      />
+      <Stack.Screen
+        name="Discounted Products"
+        options={{headerShown: true}}
+        component={DiscountedProducts}
+      />
+      <Stack.Screen
+        name="New Arrivals"
+        options={{headerShown: true}}
+        component={RecentlyAddedProducts}
+      />
+      <Stack.Screen
+        name="Search Your Products"
+        options={{headerShown: true}}
+        component={SearchScreen}
+      />
+      <Stack.Screen
+        name="Checkout"
+        options={{headerShown: true}}
+        component={CheckoutScreen}
+      />
+      <Stack.Screen
+        options={{headerShown: true}}
+        name="otp"
+        component={PhoneSignIn}
+      />
+      <Stack.Screen
+        name="Add Location"
+        options={{headerShown: true}}
+        component={LocationSearch}
+      />
+      <Stack.Screen
+        name="Submit Inquiry"
+        options={{headerShown: true}}
+        component={SubmitInquiry}
+      />
+      <Stack.Screen
+        name="Faqs"
+        options={{headerShown: true}}
+        component={faqs}
+      />
+      <Stack.Screen
+        name="Contact us"
+        options={{headerShown: true}}
+        component={Contactus}
+      />
     </Stack.Navigator>
   );
 };
